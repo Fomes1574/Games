12ea85250460ede8f20082e01e11b26b538c979f
Fomes1574 <teteumatheus1062@gmail.com>
2026-07-28 15:56:35 -0300

    add vitest.config.ts

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      reportsDirectory: 'coverage',
+      include: ['src/core/**/*.ts', 'src/domain/**/*.ts'],
+    },
+    include: ['tests/unit/**/*.test.ts', 'tests/integration/**/*.test.ts'],
+  },
+});
